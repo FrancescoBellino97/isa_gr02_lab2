6406cb6fa4c586f61516e9d03b8752ef2e042d6f
isa_gr02 <isa02@localhost.localdomain>
2020-11-18 16:13:37 +0100

    Synthesis of the mulipler with CSA

--- multiplier_input_regs/syn/scripts/first_synt_PPARCH_fpmulStage2.tcl
+++ multiplier_input_regs/syn/scripts/first_synt_PPARCH_fpmulStage2.tcl
@@ -14,10 +14,10 @@ elaborate FPmul -arch pipeline -lib WORK > ./output_report/elaborate.txt
 uniquify
 link
 
-create_clock -name MY_CLK -period 0.0 Clk
+create_clock -name MY_CLK -period 0.0 clk
 set_dont_touch_network MY_CLK
 set_clock_uncertainty 0.07 [get_clocks MY_CLK]
-set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] Clk]
+set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] clk]
 set_output_delay 0.5 -max -clock MY_CLK [all_outputs]
 set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
 set_load $OLOAD [all_outputs]
